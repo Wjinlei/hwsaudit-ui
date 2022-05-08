dc1708a5100a3cd349ca907dd44479cccb9beafd
Jerry Wang <freebufer.wang@gmail.com>
2022-05-08 12:35:59 +0800

    First commit

---
+++ src/config/router.config.js
@@ -0,0 +1,45 @@
+// eslint-disable-next-line
+import { BasicLayout } from '@/layouts'
+
+export const asyncRouterMap = [
+  {
+    path: '/',
+    name: 'index',
+    component: BasicLayout,
+    meta: { title: 'menu.home' },
+    redirect: '/dashboard',
+    children: [
+      // dashboard
+      {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: () => import('@/views/Home'),
+        meta: { title: 'menu.dashboard', keepAlive: true }
+      }
+    ]
+  },
+  {
+    path: '*',
+    redirect: '/404',
+    hidden: true
+  }
+]
+
+/**
+ * 基础路由
+ * @type { *[] }
+ */
+export const constantRouterMap = [
+  {
+    path: '/403',
+    component: () => import(/* webpackChunkName: "fail" */ '@/views/exception/403')
+  },
+  {
+    path: '/404',
+    component: () => import(/* webpackChunkName: "fail" */ '@/views/exception/404')
+  },
+  {
+    path: '/500',
+    component: () => import(/* webpackChunkName: "fail" */ '@/views/exception/500')
+  }
+]
