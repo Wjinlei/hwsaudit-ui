854da8b35b209d7705a155fec5d1c0d8eff850d1
Jerry Wang <freebufer.wang@gmail.com>
2022-05-09 10:40:36 +0800

    Config 'icons.js'

--- src/core/icons.js
+++ src/core/icons.js
@@ -6,6 +6,15 @@
  * 自定义图标加载表
  * 所有图标均从这里加载，方便管理
  */
-import bxAnaalyse from '@/assets/icons/bx-analyse.svg?inline' // path to your '*.svg?inline' file.
 
-export { bxAnaalyse }
+import fileSearch from '@/assets/icons/card/file-search.svg?inline'
+import fileSetting from '@/assets/icons/card/file-setting.svg?inline'
+
+import user from '@/assets/icons/user.svg?inline'
+import acl from '@/assets/icons/acl.svg?inline'
+import folderView from '@/assets/icons/folder-view.svg?inline'
+import permission from '@/assets/icons/permission.svg?inline'
+import profile from '@/assets/icons/profile.svg?inline'
+
+export { fileSearch, fileSetting }
+export { user, acl, folderView, permission, profile }
