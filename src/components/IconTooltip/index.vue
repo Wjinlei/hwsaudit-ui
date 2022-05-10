6262f50a9f08cd3dd2b923de1aa7d0924f27756c
Jerry <freebufer.wang@gmail.com>
2022-05-10 12:59:08 +0800

    Add 'IconTooltip' component.

---
+++ src/components/IconTooltip/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <a-tooltip :placement="placement" :overlayStyle="{ maxWidth: '800px' }">
+    <template slot="title">
+      <span><slot /></span>
+    </template>
+    <a-icon style="color: #aaafb6" type="question-circle" />
+  </a-tooltip>
+</template>
+
+<script>
+export default {
+  name: 'IconTooltip',
+  props: {
+    placement: {
+      type: String,
+      default: 'top'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+i.anticon {
+  vertical-align: -0.125em;
+  color: #3c404b;
+  margin: 0 5px;
+  cursor: pointer;
+}
+</style>
